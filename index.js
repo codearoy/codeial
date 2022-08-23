d9d954022c663288b65afccbe26ad0eaa5ecd6e7
ashishroy <ashishroy7652@gmail.com>
2022-08-23 12:50:37 +0530

    add a controller to home and also add an extra controller to users/profile

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express') ;
 const app = express() ; 
 const port = 8000 ; 
 
+// use express router  like a middle ware
+app.use('/' , require('./routes'));
+
+
+
 app.listen(port , function(err){
     if(err){
         console.log(`Error in running the server : ${port}`) ; 
