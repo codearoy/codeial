de7651f4fe2ce758ed2c4f50ba2f1db906f7672c
ashishroy <ashishroy7652@gmail.com>
2022-08-23 15:59:02 +0530

    add a view engine and install ejs

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const port = 8000 ;
 // use express router  like a middle ware
 app.use('/' , require('./routes'));
 
+// use view engine to view html file
+app.set('view engine' , 'ejs') ;
+app.set('views','./views') ; 
+
 
 
 app.listen(port , function(err){
